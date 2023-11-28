e9a58107c0cf721d8a873535ff2796e343dab076
Sanjayan Pradeep Kumar Sreekala <sanjayanps@gmail.com>
2023-11-28 10:30:24 -0800

    header files

---
+++ attention.h
@@ -0,0 +1,18 @@
+
+#ifndef ATTENTION_H
+#define ATTENTION_H
+
+// Define the bit width
+// Width of each value in the token vector
+#define BITWIDTH 4
+
+// Define the value of N
+// Number of tokens (each token is a vector)
+#define N 16
+
+// Define the value of DMODEL
+// length of a single token vector
+// Ie. each token is a vector of length DMODEL
+#define DMODEL 16
+
+#endif // ATTENTION_H
