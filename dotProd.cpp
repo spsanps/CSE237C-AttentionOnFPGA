7cd18e551547965b87e252a2f037be83a5c2b0e0
carmendyck <carmen.dyck5@gmail.com>
2023-11-29 10:43:50 -0800

    Optimize dotProd to use FF & LUTs, latency=1

--- dotProd.cpp
+++ dotProd.cpp
@@ -1,5 +1,10 @@
 #include "attention.h"
 
+data2_t mult_4bit(data_t a, data_t b)
+{
+	return a * b;
+}
+
 void dotProd(data_t row1[DMODEL], data_t row2[DMODEL], data3_t &result)
 {   
     result = 0;
@@ -9,6 +14,7 @@ void dotProd(data_t row1[DMODEL], data_t row2[DMODEL], data3_t &result)
 #pragma HLS UNROLL
 #pragma HLS ARRAY_PARTITION variable=row1 type=complete
 #pragma HLS ARRAY_PARTITION variable=row2 type=complete
-        result += row1[i] * row2[i];
+//        result += row1[i] * row2[i];
+    	result += mult_4bit(row1[i], row2[i]);
     }
 }
